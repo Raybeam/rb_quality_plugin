1de093def30a0b93e08582cdd9fadfc1fd40109e
Alex Lue <a.lue820@gmail.com>
2019-11-26 15:15:26 -0800

    formatting

--- plugins/base_data_quality_operator.py
+++ plugins/base_data_quality_operator.py
@@ -1,12 +1,17 @@
+import logging
+
 from airflow.utils.decorators import apply_defaults
 from airflow.models import BaseOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.hooks.mysql_hook import MySqlHook
 from airflow.hooks.hive_hooks import HiveServer2Hook
 
-import logging
-
 class BaseDataQualityOperator(BaseOperator):
+    """
+    Base operator that executes a data quality sql statement,
+    and optionally pushes results and metadata to an external
+    database for storage.
+    """
 
     @apply_defaults
     def __init__(self,
@@ -26,29 +31,29 @@ class BaseDataQualityOperator(BaseOperator):
         self.push_conn_type = push_conn_type
         self.sql = sql
         self.check_description = check_description
-    
+
     @staticmethod
     def _get_hook(conn_type, conn_id):
-        if conn_type == 'postgres':
+        if conn_type == "postgres":
             return PostgresHook(postgres_conn_id=conn_id)
-        elif conn_type == 'mysql':
+        if conn_type == "mysql":
             return MySqlHook(mysql_conn_id=conn_id)
-        elif conn_type == 'hive':
+        if conn_type == "hive":
             return HiveServer2Hook(hiveserver2_conn_id=conn_id)
-        else:
-            raise ValueError(f"""Connection type of {conn_type} not currently supported""")
+
+        raise ValueError(f"""Connection type of {conn_type} not currently supported""")
 
     @staticmethod
     def get_result(conn_type, conn_id, sql):
         hook = BaseDataQualityOperator._get_hook(conn_type, conn_id)
         result = hook.get_records(sql)
         if len(result) > 1:
-            logging.info("Result: " + str(result) + " contains more than 1 entry")
+            logging.info("Result: %s contains more than 1 entry", str(result))
             raise ValueError("Result from sql query contains more than 1 entry")
         if len(result) < 1:
             raise ValueError("No result returned from sql query")
         if len(result[0]) != 1:
-            logging.info("Result: " + str(result) + " does not contain exactly 1 column")
+            logging.info("Result: %s does not contain exactly 1 column", str(result[0]))
             raise ValueError("Result from sql query does not contain exactly 1 column")
         return result[0][0]
 
--- tests/test_base_data_quality_operator.py
+++ tests/test_base_data_quality_operator.py
@@ -1,17 +1,18 @@
 import pytest
-from plugins.base_data_quality_operator import BaseDataQualityOperator, PostgresHook, MySqlHook, HiveServer2Hook
+from plugins.base_data_quality_operator import BaseDataQualityOperator, \
+    PostgresHook, MySqlHook, HiveServer2Hook
 
 def test_get_result_one_result(mocker):
     mocker.patch.object(
         PostgresHook,
-        'get_records',
+        "get_records",
         return_value=[(10,)]
     )
-    
+
     result = BaseDataQualityOperator.get_result(
-        conn_type='postgres',
-        conn_id='test_id',
-        sql='SELECT COUNT(1) FROM test;'
+        conn_type="postgres",
+        conn_id="test_id",
+        sql="SELECT COUNT(1) FROM test;"
     )
 
     assert result == 10
@@ -19,49 +20,49 @@ def test_get_result_one_result(mocker):
 def test_get_result_not_one_result(mocker):
     mocker.patch.object(
         HiveServer2Hook,
-        'get_records',
+        "get_records",
         return_value=[(10,), (100,)]
     )
 
     with pytest.raises(ValueError):
         BaseDataQualityOperator.get_result(
-            conn_type='hive',
-            conn_id='test_id',
-            sql='SELECT COUNT(1) FROM test;'
+            conn_type="hive",
+            conn_id="test_id",
+            sql="SELECT COUNT(1) FROM test;"
         )
 
 def test_get_result_no_result(mocker):
     mocker.patch.object(
         HiveServer2Hook,
-        'get_records',
+        "get_records",
         return_value=[]
     )
 
     with pytest.raises(ValueError):
         BaseDataQualityOperator.get_result(
-            conn_type='hive',
-            conn_id='test_id',
-            sql='SELECT COUNT(1) FROM test;'
+            conn_type="hive",
+            conn_id="test_id",
+            sql="SELECT COUNT(1) FROM test;"
         )
 
 def test_get_result_multiple_results(mocker):
     mocker.patch.object(
         MySqlHook,
-        'get_records',
-        return_value=[(10,'bad value')]
+        "get_records",
+        return_value=[(10, "bad value")]
     )
 
     with pytest.raises(ValueError):
         BaseDataQualityOperator.get_result(
-            conn_type='mysql',
-            conn_id='test_id',
-            sql='SELECT COUNT(1) FROM test;'
+            conn_type="mysql",
+            conn_id="test_id",
+            sql="SELECT COUNT(1) FROM test;"
         )
 
-def test_get_result_invalid_connection(mocker):
+def test_get_result_invalid_connection():
     with pytest.raises(ValueError):
         BaseDataQualityOperator.get_result(
-            conn_type='invalid_type',
-            conn_id='test_id',
-            sql='SELECT COUNT(1) FROM test;'
+            conn_type="invalid_type",
+            conn_id="test_id",
+            sql="SELECT COUNT(1) FROM test;"
         )
