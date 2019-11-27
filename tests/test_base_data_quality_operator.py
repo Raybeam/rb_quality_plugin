a4c4c0b82dd6ad07e63856dd88c027b7bb8b3dd0
Alex Lue <a.lue820@gmail.com>
2019-11-27 11:49:39 -0800

    updated tests for get_result()

--- tests/test_base_data_quality_operator.py
+++ tests/test_base_data_quality_operator.py
@@ -1,6 +1,18 @@
 import pytest
+from airflow import DAG
+from datetime import datetime
 from plugins.base_data_quality_operator import BaseDataQualityOperator, \
     PostgresHook, MySqlHook, HiveServer2Hook
+
+default_args = {
+    "owner" : "airflow",
+    "start_date" : datetime(2019,11,15),
+}
+
+test_dag = DAG(
+    dag_id='test_id',
+    default_args=default_args,
+)
 
 def test_get_result_one_result(mocker):
     mocker.patch.object(
@@ -9,10 +21,17 @@ def test_get_result_one_result(mocker):
         return_value=[(10,)]
     )
 
-    result = BaseDataQualityOperator.get_result(
-        conn_type="postgres",
-        conn_id="test_id",
-        sql="SELECT COUNT(1) FROM test;"
+    task = BaseDataQualityOperator(
+        task_id="one_result_task",
+        conn_type='postgres',
+        conn_id='test_id',
+        sql='SELECT COUNT(1) FROM test;'
+    )
+
+    result = task.get_result(
+        conn_type=task.conn_type,
+        conn_id=task.conn_id,
+        sql=task.sql
     )
 
     assert result == 10
@@ -24,25 +43,39 @@ def test_get_result_not_one_result(mocker):
         return_value=[(10,), (100,)]
     )
 
+    task = BaseDataQualityOperator(
+        task_id="one_result_task",
+        conn_type='hive',
+        conn_id='test_id',
+        sql='SELECT COUNT(1) FROM test;'
+    )
+
     with pytest.raises(ValueError):
-        BaseDataQualityOperator.get_result(
-            conn_type="hive",
-            conn_id="test_id",
-            sql="SELECT COUNT(1) FROM test;"
+        task.get_result(
+            conn_type=task.conn_type,
+            conn_id=task.conn_id,
+            sql=task.sql
         )
 
 def test_get_result_no_result(mocker):
     mocker.patch.object(
-        HiveServer2Hook,
+        MySqlHook,
         "get_records",
         return_value=[]
     )
 
+    task = BaseDataQualityOperator(
+        task_id="one_result_task",
+        conn_type='mysql',
+        conn_id='test_id',
+        sql='SELECT COUNT(1) FROM test;'
+    )
+
     with pytest.raises(ValueError):
-        BaseDataQualityOperator.get_result(
-            conn_type="hive",
-            conn_id="test_id",
-            sql="SELECT COUNT(1) FROM test;"
+        task.get_result(
+            conn_type=task.conn_type,
+            conn_id=task.conn_id,
+            sql=task.sql
         )
 
 def test_get_result_multiple_results(mocker):
@@ -52,17 +85,31 @@ def test_get_result_multiple_results(mocker):
         return_value=[(10, "bad value")]
     )
 
+    task = BaseDataQualityOperator(
+        task_id="one_result_task",
+        conn_type='mysql',
+        conn_id='test_id',
+        sql='SELECT COUNT(1) FROM test;'
+    )
+
     with pytest.raises(ValueError):
-        BaseDataQualityOperator.get_result(
-            conn_type="mysql",
-            conn_id="test_id",
-            sql="SELECT COUNT(1) FROM test;"
+        task.get_result(
+            conn_type=task.conn_type,
+            conn_id=task.conn_id,
+            sql=task.sql
         )
 
 def test_get_result_invalid_connection():
+    task = BaseDataQualityOperator(
+        task_id="one_result_task",
+        conn_type='invalid_type',
+        conn_id='test_id',
+        sql='SELECT COUNT(1) FROM test;'
+    )
+
     with pytest.raises(ValueError):
-        BaseDataQualityOperator.get_result(
-            conn_type="invalid_type",
-            conn_id="test_id",
-            sql="SELECT COUNT(1) FROM test;"
+        task.get_result(
+            conn_type=task.conn_type,
+            conn_id=task.conn_id,
+            sql=task.sql
         )
