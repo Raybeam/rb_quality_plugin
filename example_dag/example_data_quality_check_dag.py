63cb7713cab71b65036ffa1d27b787e1269c00a9
Alex Lue <a.lue820@gmail.com>
2020-01-17 16:24:44 -0800

    used conf[] instead of conf.get adjustment

--- example_dag/example_data_quality_check_dag.py
+++ example_dag/example_data_quality_check_dag.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from pathlib import Path
+from collections import defaultdict
 import os
 import glob
 import yaml
@@ -9,11 +9,11 @@ from airflow.operators.data_quality_threshold_check_operator import DataQualityT
 from airflow.operators.data_quality_threshold_sql_check_operator import DataQualityThresholdSQLCheckOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-YAML_DIR = Path(__file__).parents[1] / "tests" / "configs" / "yaml_configs"
+YAML_DIR = "./tests/configs/yaml_configs"
 
 default_args = {
     "owner" : "airflow",
-    "start_date" : datetime(2020, 1, 1),
+    "start_date" : datetime(2020,1,16),
     "retries" : 1,
     "retry_delay" : timedelta(minutes=5)
 }
@@ -21,44 +21,49 @@ default_args = {
 dag = DAG(
     "data_quality_check_dag",
     default_args=default_args,
-    schedule_interval=timedelta(days=1)
+    schedule_interval="@daily"
 )
+
+def recursive_make_defaultdict(conf):
+    if isinstance(conf, dict):
+        for key in conf.keys():
+            conf[key] = recursive_make_defaultdict(conf[key])
+        return defaultdict(lambda: None, conf)
+    return conf
 
 def get_data_quality_operator(conf, dag):
     kwargs = {
-        "conn_type" : conf.get("fields").get("conn_type"),
-        "conn_id" : conf.get("fields").get("conn_id"),
-        "sql" : conf.get("fields").get("sql"),
-        "push_conn_type" : conf.get("fields").get("push_conn_type"),
-        "push_conn_id" : conf.get("fields").get("push_conn_id"),
-        "check_description" : conf.get("check_description"),
-        "notification_emails" : conf.get("notification_emails", [])
+        "conn_type" : conf["fields"]["conn_type"],
+        "conn_id" : conf["fields"]["conn_id"],
+        "sql" : conf["fields"]["sql"],
+        "push_conn_type" : conf["push_conn_type"],
+        "push_conn_id" : conf["push_conn_id"],
+        "check_description" : conf["check_description"],
+        "notification_emails" : conf["notification_emails"]
     }
-    test_class = conf.get("data_quality_class")
-    if test_class == "DataQualityThresholdSQLCheckOperator":
+
+    if conf["threshold"]["min_threshold_sql"]:
         task = DataQualityThresholdSQLCheckOperator(
-            task_id=conf.get("test_name"),
-            min_threshold_sql=conf.get("threshold").get("min_threshold_sql"),
-            max_threshold_sql=conf.get("threshold").get("max_threshold_sql"),
-            threshold_conn_type=conf.get("threshold").get("threshold_conn_type"),
-            threshold_conn_id=conf.get("threshold").get("threshold_conn_id"),
-            dag=dag,
-            **kwargs)
-    elif test_class == "DataQualityThresholdCheckOperator":
-        task = DataQualityThresholdCheckOperator(
-            task_id=conf.get("test_name"),
-            min_threshold=conf.get("threshold").get("min_threshold"),
-            max_threshold=conf.get("threshold").get("max_threshold"),
+            task_id=conf["test_name"],
+            min_threshold_sql=conf["threshold"]["min_threshold_sql"],
+            max_threshold_sql=conf["threshold"]["max_threshold_sql"],
+            threshold_conn_type=conf["threshold"]["threshold_conn_type"],
+            threshold_conn_id=conf["threshold"]["threshold_conn_id"],
             dag=dag,
             **kwargs)
     else:
-        raise ValueError(f"""Invalid Data Quality Operator class {test_class}""")
+        task = DataQualityThresholdCheckOperator(
+            task_id=conf["test_name"],
+            min_threshold=conf["threshold"]["min_threshold"],
+            max_threshold=conf["threshold"]["max_threshold"],
+            dag=dag,
+            **kwargs)
     return task
 
 data_quality_check_tasks = []
 for test_conf in glob.glob(os.path.join(str(YAML_DIR), "*.yaml")):
     with open(test_conf) as config:
-        conf = yaml.safe_load(config)
+        conf = recursive_make_defaultdict(yaml.safe_load(config))
     data_quality_check_tasks.append(get_data_quality_operator(conf, dag))
 
 task_before_dq = DummyOperator(
