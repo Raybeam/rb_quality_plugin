18ce4de0531b72ac11950b177bf028cd9b350a54
aivu <alvuong@berkeley.edu>
2020-06-23 11:49:40 -0700

    mock hook

--- tests/test_message_writer.py
+++ tests/test_message_writer.py
@@ -15,13 +15,17 @@ def test_message_writer():
 
 @mock.patch('google.cloud.bigquery_writer.Client',
             autospec=True)
-def test_bigquery_writer(mock_client):
+@mock.patch('google.cloud.bigquery_writer.Client',
+            autospec=True)
+def test_bigquery_writer(mock_client, mock_hook):
     """
     Mock the BigQuery connection and ensure that the correct info is passed
     when calling send_message().
     :param mock_client:
+    :param mock_hook:
     :return:
     """
+    mock_hook._get_credentials.return_value = 'test_creds'
     test_table_id = 'test_db.test_table_id'
     test_message = OrderedDict({"a": 1})
     mock_client.insert_rows.return_value = []
