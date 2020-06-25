d94b03a80d3006951de4dc35c571ec9ca3a1d0b7
Alina Vuong <aivu@users.noreply.github.com>
2020-06-25 08:58:15 -0700

    docstring message_writers/sql_writer.py
    Co-authored-by: Kevin McManus <kmcmanus98035@gmail.com>

--- message_writers/sql_writer.py
+++ message_writers/sql_writer.py
@@ -4,6 +4,7 @@ from rb_quality_plugin.message_writers.message_writer import MessageWriter
 class SQLWriter(MessageWriter):
     def __init__(self, table_id, connection_id, *args, **kwargs):
         """
+        Inserts provided message as a row into a SQL table.
         Note that the message's field order must match that of the table
         that it will be passed to.
 
