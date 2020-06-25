dc6db15b87aa5f6a1b67a128eb87ca0697c68c4e
aivu <alvuong@berkeley.edu>
2020-06-25 08:59:07 -0700

    docstring

--- message_writers/bigquery_writer.py
+++ message_writers/bigquery_writer.py
@@ -5,17 +5,30 @@ from rb_quality_plugin.message_writers.sql_writer import SQLWriter
 
 
 class BigQueryWriter(SQLWriter):
+    """
+    This class is used to insert a row into a BigQuery table.
+
+    An example use case would be to write an entry into a log table
+    to note the details of a completed data process--when and by whom.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def send_message(self, message):
         """
-        Inserts data into a BigQuery table.
+        Inserts data from an ordered dictionary into a BigQuery table.
 
-        Please verify that the data structure you use for the message
-        inserts rows into the BigQuery table correctly.
+        A sample message input that might be passed might be
+            {"col_1":1, "col_2":"a"}
+        for a BigQuery table with "num_rows" and "letter" as
+        the respective columns.
 
-        :param message: values to be inserted into the BigQuery table
+        Note that the keys in the dictionary do not matter--
+        only the order of values does. Therefore be sure to
+        verify that the order of the data you use for the message
+        matches the order of the columns in the BigQuery table.
+
+        :param message: ordered dictionary of values to be inserted
         :return:
         """
         message = [tuple(i[1] for i in message.items())]
